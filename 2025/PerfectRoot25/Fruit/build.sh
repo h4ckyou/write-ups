f80cfaea80a3afb84537098844a8f0db486aa0b8
Mark Uchechukwu <markuched16@gmail.com>
2025-12-08 12:51:57 +0000

    perfect root

--- 2025/PerfectRoot25/Fruit/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t chall .
-
-sudo docker run -dit --name challenge -p 1235:1235 chall
--- 2025/PerfectRoot25/Horray/challenge/setup.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-
-docker build -t heap2_chall ./
-docker stop heap2 2>/dev/null
-docker rm heap2 2>/dev/null
-docker run --restart always -itd --name heap2 --user user heap2_chall
-docker inspect heap2 | grep -i IPAddress
